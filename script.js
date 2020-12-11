59c06513949be24c38ef7d2a0eb0a73481c3d186
Luke Martin <ljmart02@gmail.com>
2020-12-11 10:10:13 -0700

    Working Youtube API call

--- script.js
+++ script.js
@@ -11,24 +11,6 @@ $(document).ready(function () {
     })
 })
 
-// Code below is for the video player
-   // This code loads the IFrame Player API code asynchronously.
-   var tag = document.createElement('script');
-
-   tag.src = "https://www.youtube.com/iframe_api";
-   var firstScriptTag = document.getElementsByTagName('script')[0];
-   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-   // This function creates an <iframe> (and YouTube player) after the API code downloads.
-   var player;
-   function onYouTubeIframeAPIReady() {
-     player = new YT.Player('player', {
-       height: '390',
-       width: '640',
-       videoId: 'EzOr8Gglf3k',
-     });
-   }
-
 $(document).ready(function() {
 
     var key = 'AIzaSyDX0T3NV-ugzJ8VlXk11vKCoCS26_2xSSs';
@@ -52,3 +34,24 @@ $(document).ready(function() {
 
 
 });
+
+
+// Code below is for the video player
+   // This code loads the IFrame Player API code asynchronously.
+   var tag = document.createElement('script');
+
+   tag.src = "https://www.youtube.com/iframe_api";
+   var firstScriptTag = document.getElementsByTagName('script')[0];
+   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+   // This function creates an <iframe> (and YouTube player) after the API code downloads.
+   var player;
+   function onYouTubeIframeAPIReady() {
+     player = new YT.Player('player', {
+       height: '390',
+       width: '640',
+       videoId: 'EzOr8Gglf3k',
+     });
+   }
+
+
