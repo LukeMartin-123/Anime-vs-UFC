71599640a4f09b1f14cee87e1c7081ba8e79d355
Brandon Grayson <brandongrayson@Brandons-Air.fios-router.home>
2020-12-11 15:06:17 -0500

    giphy connection successful

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 $(document).ready(function () {
 
-
+    
+    // BRANDONS API KEY FOR GIPHY
+    var APIKEY = 'EcTuCnxi6gDpNiUddqUXjbRwECX0iIvh'
+    // BRANDONS API KEY FOR YOUTUBE
     var apiKey = 'AIzaSyCPfeCYrxkjhyQ1ghnZO43_clhrhHxiJqs'
     var queryURL = ''
+
+    $('#giphy-test').on('click', giphy)
+
+    function giphy (event) {
+        event.preventDefault()
+        var textInput = $('#giphy-search').val()
+        console.log(textInput)
+        console.log('giphy button was clicked')
+        var url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`
+        var str = textInput.trim()
+        console.log(str)
+        url = url.concat(str)
+        console.log(url)
+        fetch(url)
+        .then(response => response.json() )
+        .then(content => {
+            // data, pagination, meta
+            console.log(content.data)
+            console.log('META', content.meta)
+            console.log(content.data[0].title)
+            var p = $('<p>')
+            var img = $('<img>')
+            var fc = $('<figcaption>')
+            img.attr('src', content.data[0].images.downsized.url)  
+            console.log(content.data[0].images.downsized.url)
+            img.attr('alt', content.data[0].title) 
+            fc.text(content.data[0].title) 
+            console.log(content.data[0].title)
+            p.append(img, fc)
+            $('#giphy-container').append(p)
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+
+
 
 
 
@@ -23,7 +65,6 @@ function animeBtn () {
 function liveActionBtn () {
     console.log('live action btn clicked')
 }
-
 
 // Code below is for the video player
    // This code loads the IFrame Player API code asynchronously.
