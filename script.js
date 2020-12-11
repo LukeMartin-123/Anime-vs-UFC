6f2115a7a61a48f0ae67e11d43b132698116e040
Luke Martin <ljmart02@gmail.com>
2020-12-11 12:36:49 -0700

    Working video player that grabs a random Youtube video from a playlist

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-$(document).ready(function () {
-    var apiKey = ''
-    var queryURL = ''
 
-    $('#anime-home-btn').on('click', function () {
-        console.log('button was clicked')
-    })
 
-    $('#liveaction-home-btn').on('click', function () {
-        console.log('button was clicked')
-    })
-})
-
+// This function calls the Youtube API and receives an object for a certain playlist
 $(document).ready(function () {
 
     var key = 'AIzaSyDX0T3NV-ugzJ8VlXk11vKCoCS26_2xSSs';
-    var playlistId = 'PLqYXv_L7NiEzgd0-1z7RmyZtirWffCZKb';
+    var playlistId = 'PLzf4erpJ2VgJ4v18XQHW5lAamvwdk-dxl';
     var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
     var options = {
         part: "snippet",
         key: key,
-        maxResults: 100,
+        maxResults: 700,
         playlistId: playlistId,
     }
 
     loadVids();
 
+    // This function takes the information from the object and places only the video ID into an array
     function loadVids() {
         $.getJSON(URL, options, function (data) {
             console.log(data)
@@ -34,20 +25,23 @@ $(document).ready(function () {
             for (i = 0; i < data.items.length; i++) {
 
                 videoIds.push(data.items[i].snippet.resourceId.videoId)
-                console.log(videoIds)
+                // console.log(videoIds)
             }
+            // This loops over the array and randomly pulls a video Id and places that ID into the video player
+            var rand = Math.random();
+            var totalVideos = videoIds.length;
+            var randIndex = Math.floor(rand * totalVideos)
+            var randomVideo = videoIds[randIndex]
+            console.log(randomVideo)
+            player = new YT.Player('player', {
+                height: '390',
+                width: '640',
+                videoId: randomVideo,
+            });
         })
     }
 
-
-
 });
-
-
-
-
-
-
 
 // Code below is for the video player
 // This code loads the IFrame Player API code asynchronously.
@@ -60,11 +54,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // This function creates an <iframe> (and YouTube player) after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'EzOr8Gglf3k',
-    });
+
 }
 
 
