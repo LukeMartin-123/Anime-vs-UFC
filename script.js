19779d40239d43c0e3239574b6fba463b9981343
Luke Martin <ljmart02@gmail.com>
2020-12-12 13:11:25 -0700

    Slight update to clean up the js code

--- script.js
+++ script.js
@@ -77,9 +77,4 @@ $(document).ready(function () {
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    // This function creates an <iframe> (and YouTube player) after the API code downloads.
-    var player;
-    function onYouTubeIframeAPIReady() {
-    }
 })
