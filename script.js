adbdb5f1f9767b05d32087f168d2afbf5b7fdcea
Luke Martin <ljmart02@gmail.com>
2020-12-12 13:04:40 -0700

    Two fully functional Youtube video players

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
-
 // This function calls the Youtube API and receives an object for a certain playlist
 $(document).ready(function () {
 
     var key = 'AIzaSyDX0T3NV-ugzJ8VlXk11vKCoCS26_2xSSs';
     var playlistId = 'PLzf4erpJ2VgJ4v18XQHW5lAamvwdk-dxl';
+    var animePlaylistId = 'PL2-sOoJcYV7YBJccS-o_qlfcaQ0liaCKq';
     var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
     var options = {
@@ -14,12 +13,19 @@ $(document).ready(function () {
         playlistId: playlistId,
     }
 
-    loadVids();
+    var animeOptions = {
+        part: "snippet",
+        key: key,
+        maxResults: 700,
+        playlistId: animePlaylistId,
+    }
 
-    // This function takes the information from the object and places only the video ID into an array
+    loadVids();
+    loadAnimeVids();
+
+    // This function takes the information from the mma video object and places only the video ID into an array
     function loadVids() {
         $.getJSON(URL, options, function (data) {
-            console.log(data)
             var videoIds = []
             var i;
             for (i = 0; i < data.items.length; i++) {
@@ -27,34 +33,53 @@ $(document).ready(function () {
                 videoIds.push(data.items[i].snippet.resourceId.videoId)
                 // console.log(videoIds)
             }
-            // This loops over the array and randomly pulls a video Id and places that ID into the video player
+            // This loops over the mmavideo array and randomly pulls a video Id and places that ID into the video player
+            var rand = Math.random();
+            var totalVideos = videoIds.length;
+            var randIndex = Math.floor(rand * totalVideos)
+            var randomVideo = videoIds[randIndex]
+            player = new YT.Player('mmaplayer', {
+                height: '360',
+                width: '480',
+                videoId: randomVideo,
+            });
+
+        })
+    }
+    // This function takes the information from the anime video object and places only the video ID into an array
+    function loadAnimeVids() {
+        $.getJSON(URL, animeOptions, function (data) {
+            console.log(data)
+            var videoIds = []
+            var i;
+            for (i = 0; i < data.items.length; i++) {
+
+                videoIds.push(data.items[i].snippet.resourceId.videoId)
+            }
+            // This loops over the animevideo array and randomly pulls a video Id and places that ID into the video player
             var rand = Math.random();
             var totalVideos = videoIds.length;
             var randIndex = Math.floor(rand * totalVideos)
             var randomVideo = videoIds[randIndex]
             console.log(randomVideo)
-            player = new YT.Player('player', {
-                height: '390',
-                width: '640',
+            player2 = new YT.Player('animeplayer', {
+                height: '360',
+                width: '480',
                 videoId: randomVideo,
             });
-        })
+        });
+    };
+
+    // Code below is for the video player
+    // This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // This function creates an <iframe> (and YouTube player) after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
     }
-
-});
-
-// Code below is for the video player
-// This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// This function creates an <iframe> (and YouTube player) after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-
-}
-
-
+})
